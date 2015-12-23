a3994f9bbca35cf14b2ca46e3e2d453608aee179
Artur_Levchenko <kami-91@mail.ru>
2015-12-24 00:40:00 +0200

    Update

---
+++ RacingCars/RacingCars/Assets/Scripts/CarBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarBehaviour : MonoBehaviour {
+
+    private Texture2D texure;
+    private float velocity = 0.01F;
+
+    // Use this for initialization
+    void Start () {
+      
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        move();
+	}
+    void move()
+    {
+        transform.Rotate(0,0,-3);
+    }
+}
