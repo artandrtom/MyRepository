ff604940db8b99c6178f31ff7d3a6548bf865b1e
Artur_Levchenko <kami-91@mail.ru>
2016-01-14 00:58:34 +0200

    Update

--- RacingCars/RacingCars/Assets/Scripts/MyClient.cs
+++ RacingCars/RacingCars/Assets/Scripts/MyClient.cs
@@ -6,47 +6,48 @@ public class MyClient : MonoBehaviour {
     private LoadBalancingClient client;
     private Room room;
     private Player player;
-    string nickname = "name";
+    string nickname = "Pidor#";
     void Start () {
-        client = new LoadBalancingClient();
-        client.AppId = "585750c50d7d4424b6606239998a33a9";
-
-        // "eu" is the European region's token
-        bool connectInProcess = client.ConnectToRegionMaster("eu");
-        room = client.CreateRoom("testRoom", new RoomOptions() { MaxPlayers = 4 });
-        client.AutoJoinLobby = true;
-        Debug.Log(connectInProcess);
-        Debug.Log(client.PlayersInRoomsCount+" "+client.PlayersOnMasterCount);
-        Debug.Log(client.CurrentServerAddress);
-        Debug.Log(client.MasterServerAddress);
-        Debug.Log(client.NameServerAddress);
-        Debug.Log("rooms: " + client.RoomsCount);
-        player = new Player(nickname, 1, true);
+        client = new LoadBalancingClient(null, "585750c50d7d4424b6606239998a33a9","1.0");
+        client.ConnectToRegionMaster("eu");
+        client.AutoJoinLobby = false;
+        room = new Room();
+        player = new Player(nickname + (int)(Random.value * 100), (int)(Random.value * 100), true);
     }
 
     void Update()
     {
-        client.Service();
+        if (client != null)
+        {
+            client.Service();  // easy but ineffective. should be refined to using dispatch every frame and sendoutgoing on demand
+        }
     }
 
     void OnGUI()
     {
-        GUI.Label(new Rect(100,50,300,150),room.ToStringFull());
-        GUI.TextField(new Rect(150, 150, 100, 40), nickname);
-        if(GUI.Button(new Rect(150, 200, 100, 50), "Join"))
+        if (client != null)
         {
-            room.AddPlayer(player);
-            Debug.Log(room.ToStringFull());
+            GUILayout.Label(client.State + " " + client.Server+ room.ToString() + " nick: " + player.NickName+" players: "+room.PlayerCount);
+            if (GUI.Button(new Rect(150, 200, 100, 50), "Join or Create"))
+            {
+
+                room = new Room("MyRoom", new RoomOptions() { MaxPlayers = 3 });
+                client.CurrentRoom = room;               
+                room.AddPlayer(player);
+                //room.StorePlayer(player);
+                Debug.Log(room.PlayerCount);
+                Debug.Log(room.IsLocalClientInside);
+            }
         }
     }
 
     void OnApplicationQuit()
     {
-        client.Disconnect();
+        if (client != null) client.Disconnect();
     }
     void MyCreateRoom(string roomName, byte maxPlayers)
     {
-        client.OpCreateRoom(roomName, new RoomOptions() { MaxPlayers = maxPlayers }, TypedLobby.Default);
+        client.OpJoinOrCreateRoom(roomName, new RoomOptions() { MaxPlayers = maxPlayers }, TypedLobby.Default);
     }
    
 }
